120f4cc4497643ec828f0529682b7922010c13e1
Pranit Bauva <pranit.bauva@gmail.com>
2015-12-09 16:10:51 +0530

    First convert the image to grayscale and then operate

--- binary_image.cpp
+++ binary_image.cpp
@@ -1,5 +1,6 @@
 #include<opencv2/core/core.hpp>
 #include<opencv2/highgui/highgui.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
 
 using namespace std;
 using namespace cv;
@@ -7,12 +8,14 @@ using namespace cv;
 int main(int argc, char **argv){
     Mat img;
     img = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+    Mat img1(img.rows, img.cols, CV_8UC1, Scalar(0));
     Mat img_binary(img.rows, img.cols, CV_8UC1, Scalar(0));
+    cvtColor(img, img1, CV_BGR2GRAY);
 
     int i, j;
     for(i = 0; i < img.rows; i++)
         for(j = 0; j < img.cols; j++)
-            if(img.at<uchar>(i, j) > 127)
+            if(img1.at<uchar>(i, j) > 127)
                 img_binary.at<uchar>(i, j) = 255;
 
     namedWindow("Normal", WINDOW_AUTOSIZE);
